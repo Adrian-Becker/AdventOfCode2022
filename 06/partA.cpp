be3b0aed74dc95717ea7ce20fa74d76a06477bf6
Adrian Becker <git@adrian-becker.de>
2022-12-08 19:13:11 +0100

    Add solution for Day 6

---
+++ 06/partA.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <array>
+#include "InputManager.h"
+
+// implemented with no fixed size for the package, so we can reuse it for part B
+
+// check that all characters in the distory are different from each other
+template<typename array>
+bool is_marker(const array &history) {
+    // inefficient, sorting would be faster
+    for (int i = 0; i < history.size() - 1; ++i) {
+        for (int j = i + 1; j < history.size(); ++j) {
+            if (history[i] == history[j]) return false;
+        }
+    }
+    return true;
+}
+
+template<typename array>
+void shift_array(array &history) {
+    for (int i = 0; i < history.size() - 1; ++i) {
+        history[i] = history[i + 1];
+    }
+}
+
+int main(int argc, char *argv[]) {
+    InputManager input(argc, argv);
+    input.next_line();
+
+    std::array<char, 4> history{' ', ' ', ' ', ' '};
+    for (int i = 0; !input.empty(); ++i) {
+        shift_array(history);
+
+        // add new signal
+        history[history.size() - 1] = input.get_char();
+
+        // check if we just read a marker
+        if (i >= (history.size() - 1) && is_marker(history)) {
+            std::cout << i + 1 << '\n';
+            break;
+        }
+    }
+}
